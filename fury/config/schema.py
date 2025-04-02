e40815eb6d9c1b04797d67d2284cc11c91db9aac
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 02:11:37 +0000

    refactor(auth): integrate database index

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_86ec65419f7c4a60a6f235de6fe8c181 = () => {
-  return '2025-03-07T02:16:56Z';
+const node_ee2048e56bcd453ea87f432e9cfa8ac8 = () => {
+  return '2025-04-02T02:11:37Z';
 };
