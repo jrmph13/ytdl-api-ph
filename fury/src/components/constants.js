aa06d2745caf73ff0b8cff8339dfdf5cb6da5528
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-02 08:49:15 +0000

    fix(core): fix payload parser

--- fury/src/components/constants.js
+++ fury/src/components/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_086ddfc0b0cc4923a56e563eeefad385 = () => {
-  return '2025-04-01T00:15:06Z';
+const node_64cc227d2a004e1b8341284d723e9cd8 = () => {
+  return '2025-04-02T08:49:15Z';
 };
