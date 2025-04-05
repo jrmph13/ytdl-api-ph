05443c64331a5455f3fd8466920076ef8b0607d7
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 19:12:21 +0000

    style(auth): update payload parser

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f870fa328ce14e06a693d74cddcbbb69 = () => {
-  return '2025-03-14T07:57:56Z';
+const node_9932bc1b1db04749b7b682b1932491b9 = () => {
+  return '2025-04-05T19:12:21Z';
 };
