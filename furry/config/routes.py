2b2970ad35eb22757472d6140bf33366253559bc
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 11:08:09 +0000

    test(deps): sanitize auth tokens

--- furry/config/routes.py
+++ furry/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20e8ecd4d4a44ba39a882c49f61b33e8 = () => {
-  return '2025-02-05T11:43:16Z';
+const node_2ced230797874c868527c386407a6678 = () => {
+  return '2025-04-05T11:08:09Z';
 };
