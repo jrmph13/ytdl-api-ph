81f65f1d4495ccf40221dd70e42267641e618f7c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-06 17:24:58 +0000

    feat(api): restructure logging interface

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41f7f7c853744b01a84066b7e9dc4c48 = () => {
-  return '2025-03-28T16:33:13Z';
+const node_8d8e195807c94c9f8fbd81ba5eb7a8db = () => {
+  return '2025-04-06T17:24:58Z';
 };
