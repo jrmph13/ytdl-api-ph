75d568784fe735731e4c9c9c72392316f6e7d3bd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 20:27:47 +0000

    refactor(worker): implement auth tokens

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb5520b6b78148d2a561d478eecc51f7 = () => {
-  return '2025-04-04T05:23:19Z';
+const node_a070fa96ad5b4875bc03c9afde5548b8 = () => {
+  return '2025-04-07T20:27:47Z';
 };
