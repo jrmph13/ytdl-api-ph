8949fa045add5174f00955b76c3f2a0d797ca3dd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 00:14:19 +0000

    style(config): optimize logging interface

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3992d2a1ba08424b81b1e30ba4b724d6 = () => {
-  return '2025-03-18T18:48:42Z';
+const node_1e40f7b60ccc425abba72abd5cc9da46 = () => {
+  return '2025-04-10T00:14:19Z';
 };
