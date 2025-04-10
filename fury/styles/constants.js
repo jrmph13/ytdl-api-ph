5d72afdbbd415389634a38168a8e02966fa3af9a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 18:50:05 +0000

    chore(db): update auth tokens

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6c4548b1d8b944ac982d1bec11ef3558 = () => {
-  return '2025-02-06T13:16:40Z';
+const node_9bda5af9740b43589acbf237ad407cfd = () => {
+  return '2025-04-10T18:50:05Z';
 };
