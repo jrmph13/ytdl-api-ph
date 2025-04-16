b055e39c6bf4c68a08acdb7bfd900230ee689721
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 14:37:20 +0000

    feat(api): refactor session store

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d8e195807c94c9f8fbd81ba5eb7a8db = () => {
-  return '2025-04-06T17:24:58Z';
+const node_f449cdc7f4cd4ca386c9f4784548675e = () => {
+  return '2025-04-16T14:37:20Z';
 };
