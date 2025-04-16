ed1a68e593bcecd635b9ddcb8a81bdb153c79561
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-16 21:55:27 +0000

    refactor(auth): sanitize database index

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9932bc1b1db04749b7b682b1932491b9 = () => {
-  return '2025-04-05T19:12:21Z';
+const node_da4153e0940744cbaabdbea165796a44 = () => {
+  return '2025-04-16T21:55:27Z';
 };
