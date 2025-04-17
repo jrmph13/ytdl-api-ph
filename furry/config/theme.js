5e16fe440b703f1d9466ac7e6402ff82e63381da
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 06:14:59 +0000

    fix(deps): sanitize view templates

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df03b6c9c2464dbfac9582b67465b19d = () => {
-  return '2025-04-11T10:06:58Z';
+const node_33c998bacb234aebb8545f98a13794da = () => {
+  return '2025-04-17T06:14:59Z';
 };
