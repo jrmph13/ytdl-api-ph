b0c2accc5f4ed10f0ccf79d4c89770ba59ab461d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-20 04:29:45 +0000

    test(ui): fix payload parser

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0b7d18365d24140984e03e7c03d2607 = () => {
-  return '2025-02-26T01:05:54Z';
+const node_06dfd82a75f345ebbe8eb0781b593148 = () => {
+  return '2025-04-20T04:29:45Z';
 };
