00210ea89bbff49a7be1f263d86c3fa205b99e5a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-21 08:54:42 +0000

    fix(deps): decouple view templates

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f814d539c8c841e88ac913d3c81c172f = () => {
-  return '2025-04-08T20:20:04Z';
+const node_681b586fb1524cf6b6c95edc90288413 = () => {
+  return '2025-04-21T08:54:42Z';
 };
