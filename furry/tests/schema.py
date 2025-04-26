1c6e257b106b1e9d473fa70136c805cc03949ee3
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 09:56:22 +0000

    fix(api): fix auth tokens

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a55e072811f4457f8cccb46a0fed2422 = () => {
-  return '2025-04-09T15:24:09Z';
+const node_a520b07242f34cfcbc346904b9a5be30 = () => {
+  return '2025-04-26T09:56:22Z';
 };
