42dd998f23cc2a213f3630eb1b751f72e49bb736
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 15:34:10 +0000

    docs(auth): implement payload parser

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab11c8e1020b41399ae0cc646723c25a = () => {
-  return '2025-02-12T16:43:14Z';
+const node_0a3a01534fa14bdabe4679443983961b = () => {
+  return '2025-04-27T15:34:10Z';
 };
