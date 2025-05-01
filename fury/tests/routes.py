5a182f600101fd2fcd37ba983d6285c56699d2cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-01 15:21:47 +0000

    chore(auth): decouple payload parser

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4ac4b14b8ef43e2bf090c6c5383e2d5 = () => {
-  return '2025-02-28T11:51:50Z';
+const node_da9b4685d1b54f36a72db526bc255751 = () => {
+  return '2025-05-01T15:21:47Z';
 };
