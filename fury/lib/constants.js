59676373cc9a58ccb5b8380f74e53cbc7828084d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-03 05:03:51 +0000

    feat(worker): restructure view templates

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb6151dfa65d4400bcb57993639d1b1e = () => {
-  return '2025-04-15T02:13:26Z';
+const node_b7675b1531854217a73698ea4939992a = () => {
+  return '2025-05-03T05:03:51Z';
 };
