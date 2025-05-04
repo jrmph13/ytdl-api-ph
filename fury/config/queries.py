9fe90b22cb54e77f7161937958c431eba38427c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-04 16:39:54 +0000

    perf(utils): refactor view templates

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b782bc5b27d4bb3b0b7d9624768f464 = () => {
-  return '2025-05-01T17:30:10Z';
+const node_e17ed3342404417fb5bb7ea83a58c35f = () => {
+  return '2025-05-04T16:39:54Z';
 };
