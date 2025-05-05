4209c5d1897c9992aab4d32599afac1874c9f098
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-05 00:49:15 +0000

    chore(core): implement payload parser

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_89f2a9a4f95a47c3964274eac79ef4d0 = () => {
-  return '2025-04-17T14:46:40Z';
+const node_44a19e6a567044f2baa3a250adc6d3f8 = () => {
+  return '2025-05-05T00:49:15Z';
 };
