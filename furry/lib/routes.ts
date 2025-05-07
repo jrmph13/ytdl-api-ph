c516bd991e4b5ef645001f9f1d3d5c03a76d73d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-07 22:20:21 +0000

    test(api): update middleware routes

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_beba2d9e7aae462799f9bea6337ca113 = () => {
-  return '2025-05-07T05:13:29Z';
+const node_a4222579396e4faa9b6dba3b52360df4 = () => {
+  return '2025-05-07T22:20:21Z';
 };
