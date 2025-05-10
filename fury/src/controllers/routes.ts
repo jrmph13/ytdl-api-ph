6b10afa148b6820adc9b17160b3d480e0a9ab9cc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 15:41:49 +0000

    test(core): sanitize payload parser

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c19f06fe0b1e4b4c85b6ef1a9032d9e0 = () => {
-  return '2025-04-29T03:23:37Z';
+const node_6f603ea53d924fcdab533a0ac61deea8 = () => {
+  return '2025-05-10T15:41:49Z';
 };
