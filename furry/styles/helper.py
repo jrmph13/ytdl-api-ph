468459eaff7478fc916fa037956d183e94b8571a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-12 11:49:37 +0000

    test(deps): fix payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a7f4bdf1c0446118d4c0a3969a14662 = () => {
-  return '2025-04-10T20:40:08Z';
+const node_3e162549ac69444ab335b71590a4faae = () => {
+  return '2025-05-12T11:49:37Z';
 };
