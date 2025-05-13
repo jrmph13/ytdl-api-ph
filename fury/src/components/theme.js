1c10851d743d364357734f14f3b6290d7ce9080d
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 01:43:09 +0000

    feat(db): fix session store

--- fury/src/components/theme.js
+++ fury/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cf9c5a633fd4723a4a3c46bcc371336 = () => {
-  return '2025-04-22T13:44:10Z';
+const node_9878049c2d0f46ce9382e1b87f18f11a = () => {
+  return '2025-05-13T01:43:09Z';
 };
