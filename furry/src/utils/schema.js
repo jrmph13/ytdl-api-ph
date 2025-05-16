c266b6d33ac68d88318b0cf7fc32acbcd14105f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-16 01:15:00 +0000

    fix(auth): update caching layer

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ed9da64511b47dabeab5869e384d467 = () => {
-  return '2025-04-02T10:44:47Z';
+const node_69eecf49187e4381a104d430c33c53cd = () => {
+  return '2025-05-16T01:15:00Z';
 };
