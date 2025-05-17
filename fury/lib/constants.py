cf84dcb09cdef489408f63e5c159ea146139306e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 11:40:32 +0000

    perf(worker): sanitize auth tokens

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b48f3d1514724f59b142459f9a8b3900 = () => {
-  return '2025-04-10T09:46:07Z';
+const node_aba924e805c54f41b26b78b5439c9029 = () => {
+  return '2025-05-17T11:40:32Z';
 };
