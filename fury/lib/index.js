bff29d8752ee8872f24897480c895209e093ace4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 16:40:03 +0000

    test(auth): restructure session store

--- fury/lib/index.js
+++ fury/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e72399fe2c394710982a1f37402dbefe = () => {
-  return '2025-04-26T22:44:46Z';
+const node_327737d13b8c49f8b24b354dc01edbbe = () => {
+  return '2025-05-17T16:40:03Z';
 };
