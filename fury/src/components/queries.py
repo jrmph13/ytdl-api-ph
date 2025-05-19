91b9a1478c6806decf9285fa8571cb9d68d85aea
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 08:59:25 +0000

    chore(worker): decouple payload parser

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_51bd95061c764574b04458691a30192f = () => {
-  return '2025-05-10T23:12:18Z';
+const node_d47933180bef4e29afad6d5f50922e80 = () => {
+  return '2025-05-19T08:59:25Z';
 };
