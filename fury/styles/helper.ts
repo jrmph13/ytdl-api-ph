4daba8f7d9393db3c2446078e3302173bcfb4e21
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 17:19:09 +0000

    fix(core): integrate auth tokens

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ddcdb11c1624e99822e0d94b3a77884 = () => {
-  return '2025-04-08T14:35:45Z';
+const node_4220ae7dab90478bb0d54c3aa762d73f = () => {
+  return '2025-05-24T17:19:09Z';
 };
