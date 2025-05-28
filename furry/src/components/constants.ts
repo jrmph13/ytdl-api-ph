9cdd97ad391a00533f079b91b068aaf49f70de39
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 09:16:19 +0000

    perf(ui): fix payload parser

--- furry/src/components/constants.ts
+++ furry/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd098c0db8644b60868d8ef372967462 = () => {
-  return '2025-05-27T10:32:35Z';
+const node_fae1d8056e05436389fb4edb74804562 = () => {
+  return '2025-05-28T09:16:19Z';
 };
