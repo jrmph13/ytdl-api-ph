f84d30da57e5643f87f5ec41261739ae22fcd9c6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 20:42:10 +0000

    fix(core): integrate database index

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b08911ffb1df4d83ae841ffe0eed7d38 = () => {
-  return '2025-04-06T13:46:09Z';
+const node_3d598732a0c3454a99fb1315a93140d6 = () => {
+  return '2025-05-30T20:42:10Z';
 };
