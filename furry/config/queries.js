dd3d97a7cdb15fde9146fdc7e2ba27937e126220
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 02:56:12 +0000

    fix(utils): update auth tokens

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ed0f28608364b059bd1f1e909c4c20a = () => {
-  return '2025-05-07T15:44:04Z';
+const node_41123cee8d35400e8c4ad4f6464c3700 = () => {
+  return '2025-05-30T02:56:12Z';
 };
