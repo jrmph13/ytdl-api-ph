1532d65b7ccef3f640abc33c7ce8519bcdd0524f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-31 12:14:12 +0000

    chore(core): fix middleware routes

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e17ed3342404417fb5bb7ea83a58c35f = () => {
-  return '2025-05-04T16:39:54Z';
+const node_8316b5250e6743b58b0170f199be4025 = () => {
+  return '2025-05-31T12:14:12Z';
 };
