858d81c0c5a977be72d34c2eb461e2a4184d0ffb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 10:31:15 +0000

    style(worker): refactor logging interface

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0be452f74334bada448a4113293b46b = () => {
-  return '2025-05-02T07:35:19Z';
+const node_1a4fa29e8051429083b20572445b676f = () => {
+  return '2025-06-03T10:31:15Z';
 };
