321b6a60acf96823d5651e7653b34b7a3a794664
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 07:52:53 +0000

    style(api): sanitize middleware routes

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b75700933e44998a870e2ca7ce61a43 = () => {
-  return '2025-04-13T15:35:37Z';
+const node_da8fc3e5021846cba7795157a8b81d80 = () => {
+  return '2025-06-06T07:52:53Z';
 };
