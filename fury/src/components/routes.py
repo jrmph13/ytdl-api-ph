57859aebbe79bf98cac2fffc29ca1daae8e9d1e2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 17:29:58 +0000

    perf(worker): optimize auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ced2aa0e9c4b49208cc675ceb686fa5a = () => {
-  return '2025-06-01T03:03:09Z';
+const node_7c19eb7ecc384b05912c7cfa41996196 = () => {
+  return '2025-06-06T17:29:58Z';
 };
