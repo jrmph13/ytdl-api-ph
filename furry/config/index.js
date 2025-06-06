2ed10615d7c06e89a49ecd1a423f67499811696e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 03:34:05 +0000

    perf(config): fix database index

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2dfe6ff7be3143b988dd0be9392819dc = () => {
-  return '2025-06-01T00:03:10Z';
+const node_e91efb866bf3412e872ddf3547a675ac = () => {
+  return '2025-06-06T03:34:05Z';
 };
