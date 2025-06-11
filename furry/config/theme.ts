f4add4a4ce226a44ee48276ef6612eac0b6fb124
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-11 04:56:40 +0000

    style(utils): update logging interface

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_afd71306731e489d8b5897fd16e037cb = () => {
-  return '2025-04-01T06:08:22Z';
+const node_933f376a66684511b7b45d03730f4851 = () => {
+  return '2025-06-11T04:56:40Z';
 };
