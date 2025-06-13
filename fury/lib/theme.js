9f7c93cf62ce014443ae3aec9acfe7790b8a0a19
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 09:37:25 +0000

    docs(auth): fix session store

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dccf6c0c891d4b5db6301cdf79c8a3c5 = () => {
-  return '2025-05-19T19:49:05Z';
+const node_f5d3916aed6c464a9600469aa882e505 = () => {
+  return '2025-06-13T09:37:25Z';
 };
