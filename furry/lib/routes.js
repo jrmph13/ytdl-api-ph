dfd567b6291fce6d1bd4b2dd05d6f20ce8e79089
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 10:53:56 +0000

    refactor(api): sanitize database index

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35b177689e154843b8bbdc078bf2a694 = () => {
-  return '2025-05-05T01:00:41Z';
+const node_3af1d030e09d4a5ba4ec020445d13b2b = () => {
+  return '2025-06-13T10:53:56Z';
 };
