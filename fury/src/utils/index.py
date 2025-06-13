7e14a076e7bcb48467358add440a3dba18eb9e7b
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 10:18:01 +0000

    test(utils): enhance auth tokens

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ba805eb3c3a447caad482f44939640e = () => {
-  return '2025-06-06T19:14:26Z';
+const node_30a8a10ed3e4482c9bb6871b10e780ed = () => {
+  return '2025-06-13T10:18:01Z';
 };
