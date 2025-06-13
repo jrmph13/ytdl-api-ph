556d13186fc76077ccf95f6e419f8bd3b6f37c4e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 17:21:18 +0000

    feat(auth): fix caching layer

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f25e3796e184dbc86ace1a2465e7616 = () => {
-  return '2025-06-02T00:26:29Z';
+const node_67740660dfd04b01bf9c07e70a1efd3f = () => {
+  return '2025-06-13T17:21:18Z';
 };
