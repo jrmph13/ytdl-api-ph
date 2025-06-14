7492f5800995e61941434a3c70e87970d73fc5f7
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 22:21:05 +0000

    chore(utils): sanitize session store

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_795b179b3b8447a9bf3e2086cf327d3a = () => {
-  return '2025-06-11T06:40:25Z';
+const node_ca0743536ce641cc86289d804a6ab21c = () => {
+  return '2025-06-14T22:21:05Z';
 };
