1cff0451a74af5403268fcec765dd167c2ff659e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 23:34:35 +0000

    feat(deps): sanitize caching layer

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_404c78e61b8145388d031124c280f684 = () => {
-  return '2025-05-29T10:25:51Z';
+const node_1cd1753621894c0496a3efa5581c74cc = () => {
+  return '2025-06-16T23:34:35Z';
 };
