834bb98a6d07c2c76bf1f253d49df27ff8178642
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-19 22:09:24 +0000

    fix(ui): enhance view templates

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34b25876c60847b188005698ad328be1 = () => {
-  return '2025-06-17T01:08:37Z';
+const node_fd90a114ac2849959d0e0d43d2fe9d10 = () => {
+  return '2025-06-19T22:09:24Z';
 };
