1da2127821324d11a4f4b1202a67cb105c527b08
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-20 09:55:22 +0000

    feat(worker): fix caching layer

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ec6cbe95716402cb93137de90425121 = () => {
-  return '2025-05-10T15:08:36Z';
+const node_4e1060370b4d4e39911c39d35996123b = () => {
+  return '2025-06-20T09:55:22Z';
 };
