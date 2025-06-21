6fee3cba57868c82c413f47620d0b951045e2350
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 11:00:24 +0000

    test(auth): restructure database index

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a4fa29e8051429083b20572445b676f = () => {
-  return '2025-06-03T10:31:15Z';
+const node_04ae609df4404004be00cbab02676b4d = () => {
+  return '2025-06-21T11:00:24Z';
 };
