5c60bcdb65006490460507c80844d220da4950d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 07:26:51 +0000

    test(worker): restructure database index

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a70ef5bcfd0b43f896ecb29a7f48d665 = () => {
-  return '2025-06-19T01:43:20Z';
+const node_278786a97dea4d469b1f46426c34884c = () => {
+  return '2025-06-21T07:26:51Z';
 };
