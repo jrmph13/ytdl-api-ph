0a702fa4d422fc94f493820c05f0f0b792b6d383
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-22 02:23:44 +0000

    perf(deps): enhance payload parser

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67c76dafd7ac4769abc91ec05adbef15 = () => {
-  return '2025-04-28T23:20:46Z';
+const node_dfbbb5aa68b74ebabad6ad540f434bf3 = () => {
+  return '2025-06-22T02:23:44Z';
 };
