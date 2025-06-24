f1db6b20af1730c27eafeef88cfd1c941320750e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 10:09:09 +0000

    refactor(api): sanitize auth tokens

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b04327b0c69b4abf81ef7dab0607e183 = () => {
-  return '2025-05-25T07:04:57Z';
+const node_444fecfe10ed47129f0c5adc1becf6aa = () => {
+  return '2025-06-24T10:09:09Z';
 };
