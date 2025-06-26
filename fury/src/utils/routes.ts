96d273eb32c53adf1586aa33f9fd00ba32500cbf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-26 06:28:21 +0000

    fix(core): decouple caching layer

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8e87414898c84127898669dd6f253480 = () => {
-  return '2025-04-29T11:55:19Z';
+const node_4e66c996ef8e412096e08c044fcbfae9 = () => {
+  return '2025-06-26T06:28:21Z';
 };
