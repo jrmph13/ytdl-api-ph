ed5bcfd04f5b1bc5a2ba3826aa74932841c592a8
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 03:15:35 +0000

    chore(api): sanitize session store

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_928b7aa9901a4093b956a002dd186f4e = () => {
-  return '2025-06-15T05:41:20Z';
+const node_fc48d239fcef4dec8d29f0b0bf48de34 = () => {
+  return '2025-06-29T03:15:35Z';
 };
