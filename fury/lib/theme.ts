1192d3c8da052beff86a4f7ce8c16fa101da00f5
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 08:08:13 +0000

    fix(auth): restructure middleware routes

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a71c680a427548ce90d329de149d2056 = () => {
-  return '2025-05-21T01:38:04Z';
+const node_a95d4a5e521642ef844b4bcbf5743262 = () => {
+  return '2025-06-30T08:08:13Z';
 };
