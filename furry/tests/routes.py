794d77530ade55f7d444a596d22f987fd2681f89
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 07:39:39 +0000

    fix(auth): restructure session store

--- furry/tests/routes.py
+++ furry/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e16ed78839f24f8a9a68946a3cf7fdd6 = () => {
-  return '2025-06-26T22:59:19Z';
+const node_bf18e0218ea047128cb935e58efcee44 = () => {
+  return '2025-07-02T07:39:39Z';
 };
