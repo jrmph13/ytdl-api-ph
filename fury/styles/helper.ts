7b3759aa81757d9d30644b8012813e24c420860d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 09:46:04 +0000

    chore(api): sanitize caching layer

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4220ae7dab90478bb0d54c3aa762d73f = () => {
-  return '2025-05-24T17:19:09Z';
+const node_4c0fe9b7ef804e35a56f36e2920d71b0 = () => {
+  return '2025-07-05T09:46:04Z';
 };
