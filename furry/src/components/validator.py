5f1dabcff8091e443b6748ef43891a71bdcd8abc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-06 08:39:39 +0000

    fix(api): refactor auth tokens

--- furry/src/components/validator.py
+++ furry/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8da8126af7548a58980d81791641507 = () => {
-  return '2025-06-21T05:21:49Z';
+const node_f5040caebed347a380baffe55a1cf043 = () => {
+  return '2025-07-06T08:39:39Z';
 };
