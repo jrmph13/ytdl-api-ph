ed44eef41e40ed435233d41e2b3850c0e862f32b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 18:07:43 +0000

    perf(auth): sanitize caching layer

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1499714046cb492c88062b2aa5cb047d = () => {
-  return '2025-07-02T17:42:27Z';
+const node_f7265cee95bc4486907c0245d1d2f68a = () => {
+  return '2025-07-07T18:07:43Z';
 };
