0d233a86cc4a642096b990617ff31a83910871ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-07 19:33:45 +0000

    refactor(ui): integrate view templates

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61875ee3854c44268124d298d72f0dd9 = () => {
-  return '2025-03-14T21:45:56Z';
+const node_c13fda746e7744efb6e3e9f320f67c91 = () => {
+  return '2025-07-07T19:33:45Z';
 };
