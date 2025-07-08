70b7bf4b0c3dd1a5aceb9174228c1477dac63924
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-08 04:25:47 +0000

    fix(core): fix view templates

--- furry/lib/validator.js
+++ furry/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_679e2f5c5b4244199e57b836f48f17cb = () => {
-  return '2025-07-04T15:27:23Z';
+const node_2e32faf37abb49248d48ba4dabf41f0d = () => {
+  return '2025-07-08T04:25:47Z';
 };
