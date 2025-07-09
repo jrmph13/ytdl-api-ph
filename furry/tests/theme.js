9209a5ba3afc3f12d17ef5b3ee3e4b80f0477ec2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-09 02:33:58 +0000

    feat(utils): refactor payload parser

--- furry/tests/theme.js
+++ furry/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c145fef62a2243fc93a47a60713d1164 = () => {
-  return '2025-06-28T00:46:22Z';
+const node_29993b6b98424101b07f6cddbbbfb8b0 = () => {
+  return '2025-07-09T02:33:58Z';
 };
