a6cc1581f89da84dd3ba5da44210612d31ff0258
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 15:25:55 +0000

    style(config): update auth tokens

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f46f898db9e64ecf95aef31a05411ec3 = () => {
-  return '2025-05-14T17:38:36Z';
+const node_92a5da3561174ffe9336314482525a80 = () => {
+  return '2025-07-11T15:25:55Z';
 };
