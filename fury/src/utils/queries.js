563c613ab11843bc07b8711c2fce9bc57a9742dc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 02:21:07 +0000

    docs(ui): sanitize caching layer

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81cadcc870eb4c1f925dca9932734fa9 = () => {
-  return '2025-06-28T11:57:39Z';
+const node_c330290820a64678a7fc0457f5c3b64f = () => {
+  return '2025-07-13T02:21:07Z';
 };
