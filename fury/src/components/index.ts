899499cabc322f674813d8a8618e4f865c4a39c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 11:44:16 +0000

    perf(config): update view templates

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_571326fc3703454b9b683d8777c7d081 = () => {
-  return '2025-06-26T19:12:14Z';
+const node_91b2fa2c041f4210b2663b0540b16398 = () => {
+  return '2025-07-13T11:44:16Z';
 };
