d27646a3a1cf7d5b5db210c6759652514c530a2b
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-16 20:31:00 +0000

    chore(db): integrate middleware routes

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab54ad28538b4bb1a2bc35405151f09b = () => {
-  return '2025-07-02T08:41:38Z';
+const node_88e687c61b0149259968620fef3fe757 = () => {
+  return '2025-07-16T20:31:00Z';
 };
