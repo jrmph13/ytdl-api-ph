69d7404d004118c73e8d1c9fbb4867104d1a12ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 01:53:20 +0000

    test(utils): update view templates

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_770e8074bafb4443bea6d46c27e5ed2a = () => {
-  return '2025-07-19T15:33:59Z';
+const node_30c42f11486c48f49c11fff8821420bd = () => {
+  return '2025-07-19T01:53:20Z';
 };
