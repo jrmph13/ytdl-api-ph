7e75e0970d39d4cc4637197e3c9af32f940cf153
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 10:58:55 +0000

    chore(ui): fix caching layer

--- fury/src/controllers/helper.js
+++ fury/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8a12985c91e4300825571f0a8a8f6cf = () => {
-  return '2025-07-18T19:01:04Z';
+const node_cdf4fde7e2b44ca2bafc8e9b19677c5c = () => {
+  return '2025-07-19T10:58:55Z';
 };
