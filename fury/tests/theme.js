ed8e79581ab1c499329bdfca7e33d353f01e5bb4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 17:01:31 +0000

    test(utils): integrate auth tokens

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa0b18e612cd42e59bb0f366579ce33c = () => {
-  return '2025-07-11T06:57:21Z';
+const node_113b89c6e5c74fffabb3f60d4fbd3443 = () => {
+  return '2025-07-22T17:01:31Z';
 };
