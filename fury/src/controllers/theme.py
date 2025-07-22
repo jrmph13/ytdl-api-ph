74b1d23a5800712465cac86cf85f7e8320a12983
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 00:21:23 +0000

    docs(auth): restructure auth tokens

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28510165c17e49799b750f9ad3397d58 = () => {
-  return '2025-06-23T09:43:28Z';
+const node_bec2c8ee52c349c3acd2f142103205ed = () => {
+  return '2025-07-22T00:21:23Z';
 };
