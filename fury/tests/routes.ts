5cb7c46beff903b000818eff37b53746105c4af5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-22 10:41:07 +0000

    fix(ui): optimize payload parser

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_41e6ca2cbd0f45dbb9bcda57bb4c6f5e = () => {
-  return '2025-06-09T13:09:25Z';
+const node_b33ca183bf0d4373ac39366c2efa1b37 = () => {
+  return '2025-07-22T10:41:07Z';
 };
