02aa81de1dace1b3e3f5277cffad64e7532cd0e8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 21:34:17 +0000

    refactor(core): enhance view templates

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c899d6651734468f9a8cea5493ba4582 = () => {
-  return '2025-07-24T11:09:35Z';
+const node_9f95651301f34e5d8022f80bac80cb3a = () => {
+  return '2025-07-25T21:34:17Z';
 };
