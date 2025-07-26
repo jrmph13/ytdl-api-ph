10b52eec49a79760bf5e4a747402df5cabb2147a
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-26 13:09:55 +0000

    test(utils): fix auth tokens

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2be7d7d9ec6d41d19883c78fbc1ce7b4 = () => {
-  return '2025-06-04T04:02:44Z';
+const node_f50caa898cf547a2b41ac0cde7b32bf0 = () => {
+  return '2025-07-26T13:09:55Z';
 };
