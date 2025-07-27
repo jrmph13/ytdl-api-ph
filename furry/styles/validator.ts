e597f0e7b72394af2fc5da79963baedbdad3b206
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 06:17:00 +0000

    refactor(auth): sanitize middleware routes

--- furry/styles/validator.ts
+++ furry/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8af364b8014742a7b04396c163ddc9fe = () => {
-  return '2025-07-08T01:36:42Z';
+const node_2a5b0da5816e47f39e1761d10bb62ca1 = () => {
+  return '2025-07-27T06:17:00Z';
 };
