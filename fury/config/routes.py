cb1f5355755c5c1cd25aa385a767447702584372
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 10:02:52 +0000

    test(config): restructure caching layer

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfb9ff7a3734421487b66107d389a8e8 = () => {
-  return '2025-07-19T07:24:10Z';
+const node_641dc33a5c044ead91c8df2fe928a6db = () => {
+  return '2025-07-28T10:02:52Z';
 };
