4999db08e11418595e4ab51c911b4da8fc15a29f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 18:04:45 +0000

    perf(core): optimize database index

--- furry/lib/routes.js
+++ furry/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3af1d030e09d4a5ba4ec020445d13b2b = () => {
-  return '2025-06-13T10:53:56Z';
+const node_c83a80a862054416b5294940a1c43f8d = () => {
+  return '2025-07-29T18:04:45Z';
 };
