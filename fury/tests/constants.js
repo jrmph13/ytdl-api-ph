b5b2253951413ab582bdbc45b7c71180e7b351f8
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 06:57:12 +0000

    feat(core): update view templates

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6bc7d9e5f1346dd87917f083357815e = () => {
-  return '2025-07-27T09:06:54Z';
+const node_33e99068424148f2a87d2f765194a5a3 = () => {
+  return '2025-07-29T06:57:12Z';
 };
