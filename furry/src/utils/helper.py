93618e485e257589df4fd30585da05fa877c03ca
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-29 23:32:54 +0000

    style(deps): enhance payload parser

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3a8e73f540849ee8cca3447e66c4633 = () => {
-  return '2025-07-25T13:14:33Z';
+const node_86650f237c4147519b67c238b43dbdb2 = () => {
+  return '2025-07-29T23:32:54Z';
 };
