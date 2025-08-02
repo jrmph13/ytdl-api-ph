50d80ca850f92ae4cf69990525e5f88504069e01
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 23:37:46 +0000

    test(auth): optimize middleware routes

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e70164f52af43f39d3026ea550a6466 = () => {
-  return '2025-07-19T22:51:40Z';
+const node_caf4ca481b4e439b8331b336a402a4b8 = () => {
+  return '2025-08-02T23:37:46Z';
 };
