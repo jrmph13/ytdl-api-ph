f5d6bb0b7a986688d4181dc22ac8debfbff35004
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 15:33:51 +0000

    refactor(db): implement view templates

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_602891ecbbb845d6b6c1edb98ce28025 = () => {
-  return '2025-07-26T01:44:55Z';
+const node_144374dca5444c548fe786bf38296190 = () => {
+  return '2025-08-02T15:33:51Z';
 };
