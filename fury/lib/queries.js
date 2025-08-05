c6601ba55c2479809c95c20f2e5ea26a93b76825
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 13:35:25 +0000

    docs(api): fix session store

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a48a97a79e784c1f91979f4754d270e1 = () => {
-  return '2025-07-18T22:51:28Z';
+const node_252a6bbe57c24016bd1c19a875a233ed = () => {
+  return '2025-08-05T13:35:25Z';
 };
