67969e3bea6bcc08899a93f77ebae22ffb401360
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 00:39:27 +0000

    style(ui): optimize session store

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5277e066d25841a3b9ddf66bb9a9fae0 = () => {
-  return '2025-06-14T18:48:19Z';
+const node_538ae27971cd421fa5cea9e38ffee41f = () => {
+  return '2025-08-07T00:39:27Z';
 };
