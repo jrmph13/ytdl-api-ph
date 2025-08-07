cf286a4a85d50bfe2d240d612b7ae3382fd68e40
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 18:46:10 +0000

    fix(ui): fix database index

--- furry/styles/constants.ts
+++ furry/styles/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9cebb55b42f444c790372a525d2ecdcd = () => {
-  return '2025-07-26T22:21:55Z';
+const node_a6840d2bb3884c7cae5aa7576deb1205 = () => {
+  return '2025-08-07T18:46:10Z';
 };
