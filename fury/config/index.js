3f99292f933548800f73a4173c4d75d20748951f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 14:58:31 +0000

    test(deps): refactor auth tokens

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e6f275778634e29a0fbfa20100a8d26 = () => {
-  return '2025-06-23T16:49:36Z';
+const node_431b2a0e8b7243aaba2365a87fb0bdce = () => {
+  return '2025-08-10T14:58:31Z';
 };
