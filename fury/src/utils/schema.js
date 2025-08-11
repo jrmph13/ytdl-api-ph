1a5269c1593212aea175b2a4792f382faf039863
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 04:49:04 +0000

    feat(core): fix database index

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_391705cd0a144d75b33b1a7a943409dc = () => {
-  return '2025-08-01T16:05:31Z';
+const node_e8a560c13b944f19906dbe0ed14794e0 = () => {
+  return '2025-08-11T04:49:04Z';
 };
