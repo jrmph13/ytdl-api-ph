974de1b7b28196271e0af6225f59c392236bba27
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 13:51:59 +0000

    fix(core): optimize payload parser

--- furry/src/components/helper.js
+++ furry/src/components/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72ca15e5df404b28a0dd85daac44178f = () => {
-  return '2025-08-05T17:26:56Z';
+const node_ee7f2b20a58e4064bf46a9d411a0e4b6 = () => {
+  return '2025-08-12T13:51:59Z';
 };
