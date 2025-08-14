52d065f440bfb849b620a27f50ed41fa4dbb4e7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 05:20:49 +0000

    perf(config): enhance database index

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3e65478f9eb2431580fa24b8069d0b36 = () => {
-  return '2025-06-13T21:38:37Z';
+const node_6de817e35792424a9b5e5c6c98422490 = () => {
+  return '2025-08-14T05:20:49Z';
 };
