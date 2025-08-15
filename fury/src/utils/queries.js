c4b6b44da12950e307da373bdab2a6cf89cf5c89
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 22:47:51 +0000

    feat(ui): decouple caching layer

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fbedf26d995e4e859b6156e9fd000e69 = () => {
-  return '2025-07-31T15:12:55Z';
+const node_1291f637e0234d69bec26093103ef3d1 = () => {
+  return '2025-08-15T22:47:51Z';
 };
