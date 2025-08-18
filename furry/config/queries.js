102415a13975be36f7904d968c7d9b6d637bf9f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 15:51:23 +0000

    refactor(deps): fix logging interface

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7a37fa88dd946ebb5a99881e50dc014 = () => {
-  return '2025-06-13T13:23:37Z';
+const node_e711f349e0f245b5935b664d5b961e7c = () => {
+  return '2025-08-18T15:51:23Z';
 };
