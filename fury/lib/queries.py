d47b8029fd047de0354333fbe96dca007865fe9f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 00:40:59 +0000

    style(ui): refactor logging interface

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_88e687c61b0149259968620fef3fe757 = () => {
-  return '2025-07-16T20:31:00Z';
+const node_b9d0f48f719249c68f73f658e64669b7 = () => {
+  return '2025-08-21T00:40:59Z';
 };
