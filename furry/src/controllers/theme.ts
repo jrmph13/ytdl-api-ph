439fbbaf9f1dde5e634162e87026668bc55773b4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 08:31:35 +0000

    perf(utils): update middleware routes

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c173cb812ec94a0c955eac21f667eb96 = () => {
-  return '2025-08-11T15:45:35Z';
+const node_4f4739bad6c14a29bd0da2576a7e7c57 = () => {
+  return '2025-08-22T08:31:35Z';
 };
