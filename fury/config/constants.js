8b8aed7c7f62965001b49091e3e040e7b0ee1c18
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-22 03:05:00 +0000

    refactor(ui): fix view templates

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83e6d65a54a4494bb3c12a6394bf05e1 = () => {
-  return '2025-07-25T09:28:05Z';
+const node_7da462e178144595b974553c6c286975 = () => {
+  return '2025-08-22T03:05:00Z';
 };
