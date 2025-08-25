9343e935209bdbe66eaba4a4370af24bb5e140b9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-25 14:49:10 +0000

    perf(ui): optimize database index

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93b5c154f4574132a6c77f290538fa2a = () => {
-  return '2025-07-17T23:59:10Z';
+const node_db388738c1f540c88c93e9e15f4f747d = () => {
+  return '2025-08-25T14:49:10Z';
 };
