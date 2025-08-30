e5fb8f65d70c7cabe19f118543d716b2b57d89f1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 22:06:16 +0000

    feat(deps): optimize database index

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a53769f6fdf409dba4d76754aa79d32 = () => {
-  return '2025-08-24T09:35:04Z';
+const node_d61eaa67d9a34e69899eecf65711b2b3 = () => {
+  return '2025-08-30T22:06:16Z';
 };
