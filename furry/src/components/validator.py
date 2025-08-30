cb2489e5b31e2fb1576a7ca4b651429e12a3ac25
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-30 05:56:28 +0000

    chore(auth): sanitize middleware routes

--- furry/src/components/validator.py
+++ furry/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_380e6de28cbe4268929987f9a3cd81df = () => {
-  return '2025-07-14T09:21:42Z';
+const node_8fa9b4f3ce7340198c28a9bbe51f9224 = () => {
+  return '2025-08-30T05:56:28Z';
 };
