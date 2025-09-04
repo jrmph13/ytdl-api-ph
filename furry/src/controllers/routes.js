0eaec668d307550a7dbe4536ce93c9cff13793ca
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-04 15:10:45 +0000

    perf(worker): fix payload parser

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cea7536d47d43578a82cc705945b876 = () => {
-  return '2025-06-24T21:24:58Z';
+const node_0ec192397760427dbaf8a81a081f5d30 = () => {
+  return '2025-09-04T15:10:45Z';
 };
