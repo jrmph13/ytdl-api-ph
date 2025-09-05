6199a2d2a2a35cd0ea2742c8a65a72a7dacdccb4
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-05 06:26:31 +0000

    test(auth): fix view templates

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e711f349e0f245b5935b664d5b961e7c = () => {
-  return '2025-08-18T15:51:23Z';
+const node_0c1cc659b79b4502a1cf1fe6879b3c0b = () => {
+  return '2025-09-05T06:26:31Z';
 };
