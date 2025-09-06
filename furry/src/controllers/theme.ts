1bd8dec890aa8c60590df26b3de7932dcde29a20
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-06 14:19:35 +0000

    docs(db): sanitize session store

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4f4739bad6c14a29bd0da2576a7e7c57 = () => {
-  return '2025-08-22T08:31:35Z';
+const node_5c480dfa2f5c47d8a33f34f5641de03a = () => {
+  return '2025-09-06T14:19:35Z';
 };
