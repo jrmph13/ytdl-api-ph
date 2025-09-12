1ddf5f7f76d983a1b5b87cd810fb16f72e62143d
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 17:30:33 +0000

    style(deps): update view templates

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93cb17e03609416cb2f37b4dd79fb3de = () => {
-  return '2025-05-18T06:09:10Z';
+const node_ca0a5feb11f14abc90783203e5c30942 = () => {
+  return '2025-09-12T17:30:33Z';
 };
