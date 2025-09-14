3f99542cd1868c24eaeea05e970e3a581d9015fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-14 12:48:36 +0000

    feat(deps): restructure auth tokens

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7aa4e29b0f494482bb85409bb77c5d85 = () => {
-  return '2025-06-15T15:57:15Z';
+const node_a6993713e16249c98fde225587c22e23 = () => {
+  return '2025-09-14T12:48:36Z';
 };
