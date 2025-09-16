8ce74cf55d0ed9e18bb4b939a0e5f7d603f64a3f
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-16 16:52:05 +0000

    docs(worker): fix view templates

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1352cfe3b508456d8c38372c879d37ab = () => {
-  return '2025-07-28T21:53:48Z';
+const node_89750b5ca3b74a90a56f0bbb21244619 = () => {
+  return '2025-09-16T16:52:05Z';
 };
