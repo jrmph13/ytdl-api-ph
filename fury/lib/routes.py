70c21544ce915ce8a9510959ef1a065cc5752691
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-17 04:01:21 +0000

    feat(utils): sanitize validation pipeline

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_42eb82a5563d405a8720c4b0d4a9c354 = () => {
-  return '2025-08-13T15:48:35Z';
+const node_c753a8f448c84b688a7e726665101e0d = () => {
+  return '2025-09-17T04:01:21Z';
 };
