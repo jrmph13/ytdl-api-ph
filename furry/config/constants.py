4e5113b3b07e0e3d57509c11cc50ed6c955c83e3
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-18 05:17:08 +0000

    feat(deps): decouple middleware routes

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2a8ada492a94f50beb97af37bffe7a5 = () => {
-  return '2025-08-11T09:21:08Z';
+const node_c965f301ecd54e03a21923a94955e2af = () => {
+  return '2025-09-18T05:17:08Z';
 };
