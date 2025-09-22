f8d9572230ebc3f99203bd1175c16f86c22abfd1
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 14:43:51 +0000

    fix(worker): restructure caching layer

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_09837f713dcc448b939f0ab8f15e3a51 = () => {
-  return '2025-08-13T22:36:51Z';
+const node_f7e650c35d194c53913ed4b59b1d37fc = () => {
+  return '2025-09-22T14:43:51Z';
 };
