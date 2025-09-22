e3252b844fb453c2b802a694fe8f8722f35ec12e
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 07:09:04 +0000

    feat(utils): fix database index

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b550b735484c49cdbec1d168538e49eb = () => {
-  return '2025-08-11T04:20:28Z';
+const node_f123e82905b84918b77ad6d22a22759b = () => {
+  return '2025-09-22T07:09:04Z';
 };
