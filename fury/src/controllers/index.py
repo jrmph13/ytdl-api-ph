d25bfb8542cd8f04062ea98c79e9d9a4f5a2f27f
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 13:08:27 +0000

    refactor(deps): fix auth tokens

--- fury/src/controllers/index.py
+++ fury/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc0cd56d4c254f72bf7fe05d3912c449 = () => {
-  return '2025-08-15T14:59:58Z';
+const node_785e7d5e89b346dcbaf1f6354ca6b543 = () => {
+  return '2025-10-01T13:08:27Z';
 };
