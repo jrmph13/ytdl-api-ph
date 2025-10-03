cd7ea4ebde835b4f19eb16a105602fde59348495
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-03 06:26:45 +0000

    fix(auth): fix logging interface

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d519dbcf4b8464781003d0b3d7bf1c2 = () => {
-  return '2025-08-13T00:26:58Z';
+const node_3669f1341e334dfdbb08512d5144cc09 = () => {
+  return '2025-10-03T06:26:45Z';
 };
