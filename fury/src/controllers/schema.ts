eacf6af5d749ec5322848f42671fa7758f432451
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-10 07:17:18 +0000

    chore(auth): enhance payload parser

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_219e80d529a144afaeded43a18bcaee7 = () => {
-  return '2025-08-07T18:44:52Z';
+const node_f622c9d8df364130b10f152d306ee987 = () => {
+  return '2025-10-10T07:17:18Z';
 };
