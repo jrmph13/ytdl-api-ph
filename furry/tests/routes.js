310e7996d8b1ddeaf2f44a289468155d001d56ed
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-14 21:56:53 +0000

    style(auth): restructure payload parser

--- furry/tests/routes.js
+++ furry/tests/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02a84be50ea54680a20cc62cf88dceaa = () => {
-  return '2025-07-10T06:10:42Z';
+const node_c5b57758019d4ceaa2c58a4b0112fde5 = () => {
+  return '2025-10-14T21:56:53Z';
 };
