f9e0e09b2b5d3a515e0e65758bcf35eb9ad50298
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-15 23:57:32 +0000

    refactor(utils): sanitize logging interface

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fbe4ba0daeb249dd84db69c9c94d43fd = () => {
-  return '2025-09-09T00:24:47Z';
+const node_b41648b001d04d729a341d3d84766fa7 = () => {
+  return '2025-10-15T23:57:32Z';
 };
