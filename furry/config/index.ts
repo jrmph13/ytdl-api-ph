3e12c18f784d7fa8e7f82b423fc95fcd1e7aac4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-21 18:57:42 +0000

    perf(ui): optimize database index

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40e1df69999f49bd9bc163f010f82fa8 = () => {
-  return '2025-07-28T05:02:46Z';
+const node_20cfdc2f875a4727a7014905a919f004 = () => {
+  return '2025-10-21T18:57:42Z';
 };
