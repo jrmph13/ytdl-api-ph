9c22635d94d20501e6e4f03e13e473231c83e1b5
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-04 21:06:15 +0000

    perf(core): fix view templates

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_431b2a0e8b7243aaba2365a87fb0bdce = () => {
-  return '2025-08-10T14:58:31Z';
+const node_4dfac65aaa9346d18bcd5dc7e456c680 = () => {
+  return '2025-11-04T21:06:15Z';
 };
