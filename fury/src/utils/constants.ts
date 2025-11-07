a52bcdaa1d3e56f8ae8bf5e390b4619eb66d2f65
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 11:57:44 +0000

    fix(auth): implement database index

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_515332ef24244447ab43d82a36bf8f13 = () => {
-  return '2025-07-14T18:49:05Z';
+const node_39ad9e4a49ac4bd598c2c3c256e026bb = () => {
+  return '2025-11-07T11:57:44Z';
 };
