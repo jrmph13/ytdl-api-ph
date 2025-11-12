5650f32a7fcd4db701ed07402bbbfc4fd41c7b69
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-12 05:04:43 +0000

    docs(api): sanitize auth tokens

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8156730bc05a4f83982cec6c4af13a68 = () => {
-  return '2025-07-11T11:22:27Z';
+const node_768770e5587b47208f01a8b7fb4200d1 = () => {
+  return '2025-11-12T05:04:43Z';
 };
