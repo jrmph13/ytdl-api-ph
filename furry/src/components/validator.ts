da55af29a64086509b0c23a2309615cb76371970
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-13 08:02:08 +0000

    chore(core): fix auth tokens

--- furry/src/components/validator.ts
+++ furry/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c6abda03f8e4cfabdbe7d95865351ed = () => {
-  return '2025-08-14T05:21:44Z';
+const node_d56b934fc947458083f225f2da85a6ba = () => {
+  return '2025-11-13T08:02:08Z';
 };
