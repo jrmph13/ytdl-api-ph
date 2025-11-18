010edb390955ce48311a27a48ea192205462c65b
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-18 23:43:05 +0000

    perf(auth): optimize view templates

--- fury/config/routes.js
+++ fury/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_61f54eeee200444194d252df612e802a = () => {
-  return '2025-10-16T23:43:36Z';
+const node_e6e4079af8c74acc8cf8b66111df06e1 = () => {
+  return '2025-11-18T23:43:05Z';
 };
