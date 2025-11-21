56837b3ac670fb0753f6b7e9459d48020870a583
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-21 11:43:31 +0000

    feat(ui): implement auth tokens

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_98c70cf00a654a6e9c347fd8ce230c57 = () => {
-  return '2025-10-24T00:52:08Z';
+const node_82036e7a2d384799a143f82ea1038549 = () => {
+  return '2025-11-21T11:43:31Z';
 };
