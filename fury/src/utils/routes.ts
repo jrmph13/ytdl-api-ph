a0f1fa4162112a3497313e6b3fc3b1e832119a7d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-25 17:22:20 +0000

    test(api): restructure auth tokens

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7993cab40aaa4efd85dfa6bb906d3284 = () => {
-  return '2025-10-26T05:26:21Z';
+const node_d7734034612c428385499c748c5c2976 = () => {
+  return '2025-11-25T17:22:20Z';
 };
