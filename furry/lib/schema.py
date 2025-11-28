284d592bd9e580248555c85e1e587b9a482c98b3
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-28 13:44:56 +0000

    perf(config): integrate payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_527d7fe79e414c63aedb43ff811c0ed5 = () => {
-  return '2025-07-30T02:24:24Z';
+const node_a2b00d2d629e4d1480750f8e87c43de7 = () => {
+  return '2025-11-28T13:44:56Z';
 };
