02bf23ab355c030917c325c7fb11fea06a7431a2
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-05 10:07:36 +0000

    refactor(auth): restructure view templates

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ceea93d14a0d4a5b8a51f38ddbf56f8f = () => {
-  return '2025-07-18T13:56:42Z';
+const node_0ed3655fa07745af82c6c63c2872ed3d = () => {
+  return '2025-12-05T10:07:36Z';
 };
