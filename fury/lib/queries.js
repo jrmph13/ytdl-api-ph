c7958c2001d02cdac505bd4900b7ebdfe72ece4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 15:31:24 +0000

    perf(core): optimize caching layer

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_252a6bbe57c24016bd1c19a875a233ed = () => {
-  return '2025-08-05T13:35:25Z';
+const node_89a8f1acd176456ea97d99425ed9e2a3 = () => {
+  return '2025-12-10T15:31:24Z';
 };
