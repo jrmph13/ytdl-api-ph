bb4b63090b4c92e65dadc077cd83a6971228e412
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 03:32:42 +0000

    feat(config): optimize database index

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f123e82905b84918b77ad6d22a22759b = () => {
-  return '2025-09-22T07:09:04Z';
+const node_4330f5eb405b44a596f5b1de4a360019 = () => {
+  return '2025-12-17T03:32:42Z';
 };
