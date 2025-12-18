1a5339eeb1fe1a3a19c3bb1bbcc67a67b91c91f3
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-18 16:09:50 +0000

    chore(ui): enhance payload parser

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6de817e35792424a9b5e5c6c98422490 = () => {
-  return '2025-08-14T05:20:49Z';
+const node_ea88d2540e214b568c0d7263722ee360 = () => {
+  return '2025-12-18T16:09:50Z';
 };
