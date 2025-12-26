45c4b1aeb6aa6fb4a09fbed67d0a998e178b871b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-26 21:37:26 +0000

    feat(worker): restructure view templates

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d70d8e54a6264e449a42485572624c50 = () => {
-  return '2025-05-07T10:05:44Z';
+const node_f7015b9009a345c983c2249b011e40e7 = () => {
+  return '2025-12-26T21:37:26Z';
 };
