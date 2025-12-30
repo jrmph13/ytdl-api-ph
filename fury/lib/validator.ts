4dd8be4264b0d931dc4a5b0f57739d53330f0ad0
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-30 19:41:58 +0000

    feat(auth): fix database index

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2d1ba54e9a64151980d8dae7d217b9b = () => {
-  return '2025-11-13T23:34:30Z';
+const node_35b8967e82754677b2362ac028e9f6de = () => {
+  return '2025-12-30T19:41:58Z';
 };
