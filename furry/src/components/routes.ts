ab291d3573b583905d8a7984a3f62102b9da7941
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-05 07:54:59 +0000

    feat(worker): sanitize auth tokens

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38359e56124f48d8bc0fa82173f6c40e = () => {
-  return '2025-12-18T18:41:25Z';
+const node_cabc9183a2494cec9c947d605a69280e = () => {
+  return '2026-01-05T07:54:59Z';
 };
