551dbf34466b54666c6a038ea3e32422537cfe21
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-08 15:23:04 +0000

    refactor(worker): decouple session store

--- fury/lib/theme.py
+++ fury/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32367d91ee2643d8a3ad601d154235ba = () => {
-  return '2025-06-01T07:48:45Z';
+const node_ee0923a974824c6aa6de7f4879cd873a = () => {
+  return '2026-01-08T15:23:04Z';
 };
