c0e4eef7f6802e921963f3ebb96ff53418603cd1
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 14:36:19 +0000

    fix(db): refactor caching layer

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_130f8584bfcc44e194fd7c77b0bc6642 = () => {
-  return '2026-01-20T01:25:37Z';
+const node_84b54d0006b34a5e9fa90aefb8c779b3 = () => {
+  return '2026-01-26T14:36:19Z';
 };
