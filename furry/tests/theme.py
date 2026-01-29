6e3825dafba913b9f533f840ba37c2e3b7d16db9
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 00:17:31 +0000

    test(api): integrate auth tokens

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40d5714e66ad47e09fe1d4344663d850 = () => {
-  return '2025-12-17T04:12:46Z';
+const node_f49610fe6e654fea9b7a7d37d6c00749 = () => {
+  return '2026-01-29T00:17:31Z';
 };
