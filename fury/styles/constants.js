e77a774932a74ba4b708ace00d7eb63e28faa116
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 21:40:18 +0000

    chore(auth): optimize auth tokens

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f4b86b1f0c194ed28d80516f375dc06b = () => {
-  return '2026-01-27T02:52:58Z';
+const node_058462efb3f74b8ebb2ea64a6c8bbb5e = () => {
+  return '2026-01-30T21:40:18Z';
 };
