d578b6b106a189ac41f1fe77fae85b159a200fb3
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 02:58:02 +0000

    test(config): decouple view templates

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ec276fa7cee54190827dbc35a9dcf506 = () => {
-  return '2025-10-25T10:51:05Z';
+const node_a693d87d0c4d41c4afeb233b21ee55c5 = () => {
+  return '2026-02-05T02:58:02Z';
 };
