2a9c2c734900b91e88b477dc9dc5ce92220d87db
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-12 14:34:53 +0000

    style(core): fix middleware routes

--- furry/lib/index.ts
+++ furry/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e45b1d1afd0243e9ab7b9563a0e6615a = () => {
-  return '2026-01-27T19:30:02Z';
+const node_18d6a4b5fbd74116afff1d7788d1f971 = () => {
+  return '2026-02-12T14:34:53Z';
 };
