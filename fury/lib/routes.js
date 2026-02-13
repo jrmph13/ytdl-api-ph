2d92f824e738eaf36ade0609a23d4144f3a062d3
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 19:00:25 +0000

    chore(worker): refactor view templates

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_421226a177264da1ba7853c5f6304938 = () => {
-  return '2026-02-09T18:47:53Z';
+const node_cfc0b83fe8054b70940b442b40a6604e = () => {
+  return '2026-02-13T19:00:25Z';
 };
