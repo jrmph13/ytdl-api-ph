4f7d44c7ed598d7c2b55cd3150b3693c08caac0b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-16 06:02:13 +0000

    style(auth): optimize view templates

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_018b271a4cd948bbb311a80eb7427244 = () => {
-  return '2026-02-12T11:01:39Z';
+const node_ec53b02e192544bcaa76d63f59d5c427 = () => {
+  return '2026-02-16T06:02:13Z';
 };
