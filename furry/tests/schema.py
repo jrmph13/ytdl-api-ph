275d957920e7b9f4aa63c7377c9f8c3581e2641b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 17:26:26 +0000

    feat(deps): decouple payload parser

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dcc35344a2d747688e4f166ddd38a46e = () => {
-  return '2026-02-04T08:21:42Z';
+const node_6c49e9a35a264f9e96afbb15340a1872 = () => {
+  return '2026-02-18T17:26:26Z';
 };
