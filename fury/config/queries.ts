5093e84343d54a6ca4edd8e4ccdf61989533b83f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-05 13:23:22 +0000

    style(db): decouple auth tokens

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_198d5f26cf5e4720a713b090e1aed98c = () => {
-  return '2025-07-31T02:42:54Z';
+const node_aebaba23a61f408f87dc340cc9b4d3c8 = () => {
+  return '2026-03-05T13:23:22Z';
 };
