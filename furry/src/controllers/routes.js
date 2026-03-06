d0eadffa686eafc92badd4e6ab746b6a5e798db7
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 01:30:31 +0000

    fix(ui): decouple auth tokens

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fdcde129413648749fd8c46f578a72d1 = () => {
-  return '2026-02-25T06:22:35Z';
+const node_942ec149ff3c47c3bf24e94c853815dd = () => {
+  return '2026-03-06T01:30:31Z';
 };
