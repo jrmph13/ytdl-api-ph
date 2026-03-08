79e7c73bad14b0648386786051c4f8c08a48a984
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 16:42:54 +0000

    refactor(api): restructure view templates

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f14c7d2db7434524bdcdb83893dab705 = () => {
-  return '2025-12-16T15:48:07Z';
+const node_396dcfa880af40278675638caca38387 = () => {
+  return '2026-03-08T16:42:54Z';
 };
