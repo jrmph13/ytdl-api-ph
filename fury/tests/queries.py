b20e703e8944bba5237ca412c5a03068ba82357e
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 06:55:45 +0000

    refactor(api): refactor middleware routes

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cabcf60c26c346ad8d2e2f9d01b7a065 = () => {
-  return '2026-02-16T11:35:32Z';
+const node_a0a6d447ec2f4d08a68d7783346c8a0f = () => {
+  return '2026-03-10T06:55:45Z';
 };
