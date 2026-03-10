898aa3fbc5a5d434b9f7e7a3a40b549ad6dd444d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 17:36:22 +0000

    style(auth): decouple auth tokens

--- fury/src/components/theme.ts
+++ fury/src/components/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b87181fc88043d38c95b634663a4829 = () => {
-  return '2026-01-24T12:26:26Z';
+const node_e6e7e3b9e4a6443f93ca03095131aa93 = () => {
+  return '2026-03-10T17:36:22Z';
 };
