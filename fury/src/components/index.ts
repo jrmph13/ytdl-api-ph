d36b97335be95282b0e361b86a93b6cce28ce36c
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 20:33:02 +0000

    test(auth): integrate auth tokens

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4721ad5a18641a6a3c09d68ddc0bd7d = () => {
-  return '2026-01-28T15:41:06Z';
+const node_8dab49eecdf24da294b86d33d81b299d = () => {
+  return '2026-03-11T20:33:02Z';
 };
