5f129509c8263a1bfe5567b5c8fd6aaa64d09f55
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 12:34:09 +0000

    feat(deps): enhance auth tokens

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7b969477818746c5850cc34d8e75112e = () => {
-  return '2026-02-05T07:39:35Z';
+const node_03389a66a6b54db4953f895609a3de15 = () => {
+  return '2026-03-11T12:34:09Z';
 };
