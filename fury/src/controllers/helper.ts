79f43de48ff550a7e6814ff5a159f548f2ab6bb3
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 08:08:36 +0000

    fix(worker): restructure view templates

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47a6a947d682460d8859382848c9ecc0 = () => {
-  return '2025-09-25T08:39:43Z';
+const node_e201d9370cc24eddb4f7e35529347004 = () => {
+  return '2026-03-14T08:08:36Z';
 };
