fafa7b87d107bc4ae6944471c17fae52c6e8d803
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-14 10:44:37 +0000

    refactor(config): sanitize session store

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2a63f01896349268b0cc841d8d0c7db = () => {
-  return '2026-03-02T04:07:50Z';
+const node_4a04cc974433433b816d598030411b87 = () => {
+  return '2026-03-14T10:44:37Z';
 };
