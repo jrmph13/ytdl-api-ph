ad87c93825c5de16cfd269c7f8cd36be6d8dbe96
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-16 23:35:35 +0000

    test(deps): decouple logging interface

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_606836af50c04846b18780a053da432b = () => {
-  return '2026-02-23T00:05:52Z';
+const node_4e4a50de89964ce0a94edd6b5c53ec20 = () => {
+  return '2026-03-16T23:35:35Z';
 };
