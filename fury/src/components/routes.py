8fe6df8a980203feb144c39cc28897d57d7c300c
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-17 13:17:40 +0000

    test(auth): restructure middleware routes

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_808f023872544b4c9e4f2870b3feec30 = () => {
-  return '2025-09-14T09:12:02Z';
+const node_71ae7505d5bd41cea73d8cc9e7c46688 = () => {
+  return '2026-03-17T13:17:40Z';
 };
