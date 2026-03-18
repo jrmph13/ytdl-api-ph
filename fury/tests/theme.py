7b438514ab67a3e6f2671f00be0e6a6f100d2a9d
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-18 20:03:44 +0000

    style(ui): decouple middleware routes

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5568f2d45eb04e54a8a44a71defb3b97 = () => {
-  return '2026-03-18T01:57:33Z';
+const node_13a5eb02b3b14ee5a18e022760c78097 = () => {
+  return '2026-03-18T20:03:44Z';
 };
