c8731c5156add5e655c5339b7dc0833f80b433f5
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-20 15:26:31 +0000

    fix(auth): sanitize database index

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20b6b36e70904ed7937ad11642a39d41 = () => {
-  return '2026-03-15T08:34:55Z';
+const node_af540225733640a59c26bd8e086ca7e8 = () => {
+  return '2026-03-20T15:26:31Z';
 };
