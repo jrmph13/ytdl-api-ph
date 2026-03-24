aa97094e30606d583d8c38f2bb87ca485aa7fd99
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-24 19:10:16 +0000

    style(auth): sanitize middleware routes

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8981ff6038ea49c690f34c466c3b877b = () => {
-  return '2026-02-25T19:17:57Z';
+const node_a67fffafedef484a841171ac97e4765b = () => {
+  return '2026-03-24T19:10:16Z';
 };
