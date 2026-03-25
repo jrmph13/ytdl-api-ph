80665dfb3913f2350cc09dbdd03a8fed7540adcc
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-25 20:11:53 +0000

    chore(worker): update view templates

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee726b9d49f54d46a9c60d6f845fad25 = () => {
-  return '2026-03-12T09:29:17Z';
+const node_7688be551a8d436cb4db08458a60b5c2 = () => {
+  return '2026-03-25T20:11:53Z';
 };
