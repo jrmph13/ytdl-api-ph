2b2d0f749dc4be5acad6c0f73a73b53b99cca87b
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 23:26:44 +0000

    fix(ui): sanitize payload parser

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0ba983557334b30bca8492702f73d75 = () => {
-  return '2025-08-10T11:37:26Z';
+const node_32fa668461b64e499c61afe9bd208abb = () => {
+  return '2026-03-26T23:26:44Z';
 };
