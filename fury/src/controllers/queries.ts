bb8d740fa35ee8d6d797beeb8a9e06e27dbde909
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-27 03:31:09 +0000

    fix(core): update session store

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d85d1f026408484ebad56facaa109e28 = () => {
-  return '2026-03-15T14:24:41Z';
+const node_a17c3327d1a848ed90dccb3c21b1d793 = () => {
+  return '2026-03-27T03:31:09Z';
 };
