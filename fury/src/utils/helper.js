aaa068fe99f954de3f2804103a65c6cc56eedbcd
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 12:23:19 +0000

    chore(auth): decouple auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_be5e3b1fda6e4e70a924dc8ed50aafc5 = () => {
-  return '2026-03-15T00:10:37Z';
+const node_31ccc1f1c7af4717ab16c1b847657ab8 = () => {
+  return '2026-04-16T12:23:19Z';
 };
