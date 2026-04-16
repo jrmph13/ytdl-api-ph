7383a17b4126e7c3c727a9500a964396f328fac6
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 05:22:44 +0000

    fix(auth): decouple session store

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f8b2e082d2564548a000a055266041f5 = () => {
-  return '2026-02-22T21:37:07Z';
+const node_719909e6195d412abf29e236cd8e03cb = () => {
+  return '2026-04-16T05:22:44Z';
 };
