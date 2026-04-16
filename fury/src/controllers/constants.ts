5b227b02696a6f94866b002d55d7121b69b129a8
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-16 14:05:29 +0000

    feat(worker): restructure caching layer

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4557a108c4ad4cadbb3d3cd05a84fb7d = () => {
-  return '2026-04-03T01:38:24Z';
+const node_28213d7fa0e849348df73fdbfd9e5f45 = () => {
+  return '2026-04-16T14:05:29Z';
 };
