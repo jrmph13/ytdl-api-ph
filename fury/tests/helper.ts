905c71ac818a07e9d2aaab8fb3b326a802d63df8
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 22:35:43 +0000

    refactor(worker): implement middleware routes

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8501890ef35343238f614df1c261ef35 = () => {
-  return '2026-04-16T03:25:57Z';
+const node_e92414d58d9044329e2ec924dc157f4d = () => {
+  return '2026-04-22T22:35:43Z';
 };
