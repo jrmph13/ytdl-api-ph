6c48377ae2f0937d7339782cadf982a19efe2939
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 20:28:54 +0000

    feat(api): enhance payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_03389a66a6b54db4953f895609a3de15 = () => {
-  return '2026-03-11T12:34:09Z';
+const node_d8c32a89c5904ee6af2010c39c892439 = () => {
+  return '2026-04-22T20:28:54Z';
 };
