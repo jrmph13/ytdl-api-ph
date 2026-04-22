5a65ce83844fd36efe7c825c96ff767efabf164a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-22 12:46:32 +0000

    feat(api): enhance middleware routes

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7438b1f0d7bc4dcfba40389338444ed6 = () => {
-  return '2026-04-03T08:18:21Z';
+const node_f81e66772c8d443aa8afb4e1a3b5ef04 = () => {
+  return '2026-04-22T12:46:32Z';
 };
