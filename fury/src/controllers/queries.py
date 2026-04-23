74397512b7706fe6e211807f80625885dc48c3a9
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 12:41:27 +0000

    chore(utils): refactor middleware routes

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de488dcb666d475aac5f3e62b8a8e03e = () => {
-  return '2026-02-25T23:00:43Z';
+const node_ce63ba16655b47e8ba50bd309fb40cb0 = () => {
+  return '2026-04-23T12:41:27Z';
 };
