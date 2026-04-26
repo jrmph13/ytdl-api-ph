f33b633bce98e6ed2894b73f278e7cb3a7c75f36
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-26 02:21:14 +0000

    feat(auth): implement auth tokens

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef4e250020f34af3b485c97a07daf4eb = () => {
-  return '2026-04-15T20:01:44Z';
+const node_2b47683fda5b4b6b87c2fc204d51918c = () => {
+  return '2026-04-26T02:21:14Z';
 };
