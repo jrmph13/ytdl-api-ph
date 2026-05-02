501feb87ce6f480ac8aa23426303f54de3767c73
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 16:03:09 +0000

    fix(config): decouple payload parser

--- fury/tests/validator.js
+++ fury/tests/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1ebaf7118e6149aa85c6cc39673420a4 = () => {
-  return '2026-04-14T12:14:21Z';
+const node_d7ddb928aa8242338ff9455ca82bbb33 = () => {
+  return '2026-05-02T16:03:09Z';
 };
