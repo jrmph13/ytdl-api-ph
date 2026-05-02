d0e5c3355bb1ffdb797ed0c9895e1b39ac3694d6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-02 12:48:03 +0000

    refactor(ui): implement auth tokens

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef6c781e4b45496995c26b333ec8abbd = () => {
-  return '2026-04-26T13:50:10Z';
+const node_097f91f4ba6c45b8ac34155bed4a6098 = () => {
+  return '2026-05-02T12:48:03Z';
 };
