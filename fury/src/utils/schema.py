779382da16fdf281b484a56363910fce3363f95c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 18:49:29 +0000

    refactor(auth): sanitize validation pipeline

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2281c27578024d9cae0f056a80a2e175 = () => {
-  return '2026-03-15T07:11:32Z';
+const node_d92df359664648679d781fb709e4ff29 = () => {
+  return '2026-05-07T18:49:29Z';
 };
