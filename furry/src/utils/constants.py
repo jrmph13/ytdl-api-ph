28b5ead92c3be5e6ec70c83373fd706274ba1e2f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 00:54:33 +0000

    refactor(ui): restructure auth tokens

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2c54a168b97644c088a43fd76f153340 = () => {
-  return '2026-04-27T20:26:28Z';
+const node_dd1ff690b7994afca7e1c4ba58309bee = () => {
+  return '2026-05-07T00:54:33Z';
 };
