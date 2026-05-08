2af7f780039a6c3b7502e2ab6142d2283fb1191c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 07:36:05 +0000

    perf(api): integrate payload parser

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_64d4d6fb603f42fe9553c9d55dde9d92 = () => {
-  return '2026-04-14T23:01:26Z';
+const node_bd1132f0390e4e2ca5eb4df07f092595 = () => {
+  return '2026-05-08T07:36:05Z';
 };
