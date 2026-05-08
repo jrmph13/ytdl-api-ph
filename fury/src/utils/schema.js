a7eeebe3d73c941e8d0acef842a4d2e807e8597d
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 01:22:28 +0000

    refactor(db): optimize database index

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58ed1c527b714381b6c7e98d510e4dc9 = () => {
-  return '2026-04-11T07:16:25Z';
+const node_ebd3ed7b24174430a932fee1221525ff = () => {
+  return '2026-05-08T01:22:28Z';
 };
