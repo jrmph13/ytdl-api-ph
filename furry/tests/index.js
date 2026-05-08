3ef074bd76514d17bd7afddb138f6535eab7b4e2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-08 18:52:38 +0000

    test(api): decouple payload parser

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37ac626828e64fc79a12a183018fc6b1 = () => {
-  return '2026-03-22T08:42:04Z';
+const node_09d290a8e0ed470eaf450f4b92290422 = () => {
+  return '2026-05-08T18:52:38Z';
 };
