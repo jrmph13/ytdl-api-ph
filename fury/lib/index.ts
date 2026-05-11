6715ecaefa3513abb6eaaa7e08536cf000975d6f
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 18:27:08 +0000

    docs(worker): restructure view templates

--- fury/lib/index.ts
+++ fury/lib/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_504d043149504a759574731f245f3af6 = () => {
-  return '2026-05-02T04:08:06Z';
+const node_3e51e457e85f43788e9ec9bb9fe02417 = () => {
+  return '2026-05-11T18:27:08Z';
 };
