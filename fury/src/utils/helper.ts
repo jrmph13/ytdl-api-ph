b8be598988e8a660e6521db1bb7dc0d0221115f4
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 20:09:15 +0000

    perf(api): update logging interface

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bdba6db0fc034406b44d46230f06dd05 = () => {
-  return '2026-02-12T09:48:46Z';
+const node_5e7d5dc92eb7412ba766a3e7995e568a = () => {
+  return '2026-05-11T20:09:15Z';
 };
