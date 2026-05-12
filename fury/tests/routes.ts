d8d3707cee6b51ddb6c708a2af1a405da18c4826
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 23:14:28 +0000

    feat(worker): update middleware routes

--- fury/tests/routes.ts
+++ fury/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0ad0daef86ad46a385d604343af2ab4a = () => {
-  return '2026-04-13T01:29:24Z';
+const node_b6c64ade190749fe878317d2c3a307f7 = () => {
+  return '2026-05-12T23:14:28Z';
 };
