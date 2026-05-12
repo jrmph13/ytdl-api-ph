59a0b5614b70b28a6af7224ab0de0eaf855a5244
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 11:42:46 +0000

    style(core): fix view templates

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e0cf4c4dc2c7463b8e9c89e8da98c050 = () => {
-  return '2026-03-08T01:16:18Z';
+const node_a5792ac9df8b4f1ab132952e530cb3ad = () => {
+  return '2026-05-12T11:42:46Z';
 };
