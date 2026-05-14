56287b2bf2b8f4b18cb003ebb8e7af90d15d7266
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-14 01:31:17 +0000

    fix(config): decouple validation pipeline

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6814cd6e4cc24cb498473d6b0f3375aa = () => {
-  return '2026-04-22T00:34:54Z';
+const node_f78bacb6bb7d4637a85c4c115c693224 = () => {
+  return '2026-05-14T01:31:17Z';
 };
