b0b22d105b5a3b4fb55a1621a590f444dd1ea4f9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-21 05:23:53 +0000

    style(utils): fix view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22cb9375a1834fada92b6f0feb2eb2b2 = () => {
-  return '2026-04-14T08:09:13Z';
+const node_13441cfc9a0642619f100e866e2a8fdd = () => {
+  return '2026-05-21T05:23:53Z';
 };
