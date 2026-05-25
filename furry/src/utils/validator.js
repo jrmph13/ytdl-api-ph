d868ad6d4a98dc2a65f4c4758c601355a7341349
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-25 08:14:00 +0000

    refactor(utils): decouple payload parser

--- furry/src/utils/validator.js
+++ furry/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f97d4052b2634713823144c661f90440 = () => {
-  return '2026-05-14T20:45:34Z';
+const node_c83fa13282284684964b11e3c071bc58 = () => {
+  return '2026-05-25T08:14:00Z';
 };
