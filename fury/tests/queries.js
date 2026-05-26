af1292eaf14f469edcfcda621a9ec2001ea96c2b
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 17:08:26 +0000

    fix(api): update caching layer

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f81e66772c8d443aa8afb4e1a3b5ef04 = () => {
-  return '2026-04-22T12:46:32Z';
+const node_ceff3555b74a4668b6fd8a1b93e55924 = () => {
+  return '2026-05-26T17:08:26Z';
 };
