eaf5e5e69c612a26cc05747e0566490746bb17de
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 07:24:45 +0000

    feat(auth): restructure auth tokens

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ebac9d19fc74f3b90134b704359ba42 = () => {
-  return '2026-05-06T19:03:00Z';
+const node_91e55a417bc04b62ba95e0a52fd15db9 = () => {
+  return '2026-05-26T07:24:45Z';
 };
