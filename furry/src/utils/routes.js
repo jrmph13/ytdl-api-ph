15ac27f0757daad23106707611dc4d2ae1ac510c
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 23:42:07 +0000

    refactor(config): decouple auth tokens

--- furry/src/utils/routes.js
+++ furry/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_501968c713684a4584871d8e05b9d273 = () => {
-  return '2026-04-11T07:20:51Z';
+const node_a72faaaf2e7a459d865dc7444f16184f = () => {
+  return '2026-05-26T23:42:07Z';
 };
