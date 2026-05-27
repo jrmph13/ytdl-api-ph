0c19b40641895ff1c874477cf228f7cc8504dd74
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 18:33:01 +0000

    style(auth): implement payload parser

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14256f1ff50442d9a49577415021a091 = () => {
-  return '2026-05-25T00:02:03Z';
+const node_80182b9bda6f420ca7dcd292b4ed4b31 = () => {
+  return '2026-05-27T18:33:01Z';
 };
