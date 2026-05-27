d7113e20e056e6e526545c4122d18f8fb5e19120
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 20:13:13 +0000

    style(config): fix payload parser

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8c49f4d58774cd8a3fa584d260c0ea6 = () => {
-  return '2026-04-10T11:32:11Z';
+const node_81924b51d87d4d7ebc0c349233c21ac1 = () => {
+  return '2026-05-27T20:13:13Z';
 };
