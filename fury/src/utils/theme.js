cb3fa7b53e8d7abd60a511f680fba0d2573d6266
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 18:39:37 +0000

    fix(auth): implement view templates

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8bcf19c6bb464667ad39554d21a0f18d = () => {
-  return '2026-05-26T15:51:37Z';
+const node_5da084cd1899486e9b29c4b05a007b94 = () => {
+  return '2026-06-04T18:39:37Z';
 };
