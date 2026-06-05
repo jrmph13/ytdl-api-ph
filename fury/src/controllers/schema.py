f451b763e2977b1d41953ebd526d07b0eb13bc2e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 21:27:46 +0000

    chore(db): fix auth tokens

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_26dd02e93ab14ae79be669d20db31524 = () => {
-  return '2026-04-26T00:10:26Z';
+const node_5fd93b2ba0974adcb9086c45ad502e49 = () => {
+  return '2026-06-05T21:27:46Z';
 };
