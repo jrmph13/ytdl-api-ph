ee4ed1c65c1326ea15a45ac57f759886cc76af61
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-06 11:59:14 +0000

    style(deps): optimize view templates

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6de290b45dfb4639a62f7e9a82aaab61 = () => {
-  return '2026-02-22T01:42:36Z';
+const node_71313a00c5704fb4a6ce9d5d015938ed = () => {
+  return '2026-06-06T11:59:14Z';
 };
