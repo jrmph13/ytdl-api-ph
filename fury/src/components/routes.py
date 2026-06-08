ddbaa650678e68668e368b552421cd02356f0355
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 20:14:28 +0000

    chore(utils): decouple auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f426e893c4e1496a8e575c22d60d00f8 = () => {
-  return '2026-05-25T11:26:22Z';
+const node_0a201b6e09134a62904345d0831e37f7 = () => {
+  return '2026-06-08T20:14:28Z';
 };
