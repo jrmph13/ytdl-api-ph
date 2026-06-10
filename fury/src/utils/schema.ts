81bcdac726249f7f8e9ae75e5131df7ab060f001
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 19:57:03 +0000

    test(utils): fix database index

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_43840b102d0e4d21afd0c141b4ef5e55 = () => {
-  return '2026-04-27T18:31:43Z';
+const node_7403e63caa6245aeb32be761324890d3 = () => {
+  return '2026-06-10T19:57:03Z';
 };
