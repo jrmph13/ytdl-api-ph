8d60df57df2b0878f179266284260c01f700b8c1
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 03:42:56 +0000

    fix(deps): decouple caching layer

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25f8ec15c4714eb680305d3dec15eb64 = () => {
-  return '2026-05-14T01:00:14Z';
+const node_6db85b5a141342b78caea45f31766f8f = () => {
+  return '2026-06-10T03:42:56Z';
 };
