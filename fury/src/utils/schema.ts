70f452f0b96cfd271e9cffc808c092f70a292639
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 14:55:40 +0000

    fix(utils): integrate middleware routes

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7403e63caa6245aeb32be761324890d3 = () => {
-  return '2026-06-10T19:57:03Z';
+const node_259c2c947ac040c1a6a71470ec44b9ef = () => {
+  return '2026-06-11T14:55:40Z';
 };
