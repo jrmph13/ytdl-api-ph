234aa96e349ebc355b0ca0239bd59cc2be1ea244
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 07:40:41 +0000

    perf(config): fix auth tokens

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70b95df429e54e0381922dd8c76617f4 = () => {
-  return '2026-02-20T23:04:21Z';
+const node_8b8cbd0444c94841813034f5bb6f42b4 = () => {
+  return '2026-06-11T07:40:41Z';
 };
