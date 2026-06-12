ed6ade6bc57c2fdf2125583b41287de4659935c4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-12 18:43:20 +0000

    fix(auth): sanitize logging interface

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3c9e3f56d144ead81e36135998737ae = () => {
-  return '2026-05-19T03:12:10Z';
+const node_1c1a2cf71b6241978b737e20ac1b33fa = () => {
+  return '2026-06-12T18:43:20Z';
 };
