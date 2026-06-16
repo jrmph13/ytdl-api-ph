a25876814a012783c9178ed2738efec1e0ed5dbf
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 22:06:01 +0000

    fix(utils): refactor view templates

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d2de011a94e40b084e10b183a100775 = () => {
-  return '2026-04-15T07:19:06Z';
+const node_5b4e0c80326f42ee89d910adcf465d14 = () => {
+  return '2026-06-16T22:06:01Z';
 };
