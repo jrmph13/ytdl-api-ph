dfe078291138d22e4723c678b4bded47e2360922
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 15:50:03 +0000

    fix(utils): sanitize payload parser

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3af3940364144748be476653d2691106 = () => {
-  return '2026-05-25T11:23:35Z';
+const node_208ba7e2fca14561a1e3cad339e6b9bc = () => {
+  return '2026-06-16T15:50:03Z';
 };
