33c79b2dc86bf9615b9d4f538f85f9a02e691830
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 17:05:04 +0000

    fix(worker): restructure database index

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d2186c89277f40bbbfa597dcb35eb1f2 = () => {
-  return '2026-06-10T06:51:49Z';
+const node_20fe5a6c0cd9453e9175054f2025d286 = () => {
+  return '2026-06-17T17:05:04Z';
 };
