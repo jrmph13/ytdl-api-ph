6bc988a257a258c02302fdd548d84cf07cb7de1e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 03:38:22 +0000

    perf(core): update logging interface

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_482fabc3151b4d67ad81e95a2d8558b1 = () => {
-  return '2026-06-16T09:30:14Z';
+const node_44edbef4b2634db187af5a5bc271c1d4 = () => {
+  return '2026-06-19T03:38:22Z';
 };
