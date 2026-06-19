3b5392c1d892e5dbae6185568e3bcdd0acf11b66
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 18:43:26 +0000

    test(api): enhance middleware routes

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d4ef6b16f11f40e7bb7ae91947e9f286 = () => {
-  return '2026-04-22T19:19:29Z';
+const node_ebc645319c424d9d9dd0ab4d775857e6 = () => {
+  return '2026-06-19T18:43:26Z';
 };
