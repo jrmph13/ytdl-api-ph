2432fe7c70b5d6de2f9ee94eb2d6d87fe98d82e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-20 18:57:51 +0000

    fix(utils): fix validation pipeline

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f626a9715d3463284c497c403849f3c = () => {
-  return '2026-02-25T19:25:26Z';
+const node_ade374b3f9484903b12d50e006f7c685 = () => {
+  return '2026-06-20T18:57:51Z';
 };
