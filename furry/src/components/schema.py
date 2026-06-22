0770a9522c7e89668b792c9dee57786d7e143602
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 19:36:54 +0000

    test(api): decouple payload parser

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9913bb60e4ea45549ad89fef425ce5f6 = () => {
-  return '2026-06-08T06:13:33Z';
+const node_5b840aac2c4f4252bd32387ce8083b7a = () => {
+  return '2026-06-22T19:36:54Z';
 };
