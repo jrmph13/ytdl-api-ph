00feadaad68e54cfb4b306fab110f76148e19da2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 05:41:48 +0000

    perf(db): sanitize auth tokens

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fc15eba216814c0aa9bfb4b18539c6a4 = () => {
-  return '2026-06-18T13:01:29Z';
+const node_e0b024d26f0745c1908a4541ec34cc6b = () => {
+  return '2026-06-25T05:41:48Z';
 };
