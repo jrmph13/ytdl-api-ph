be013db29f68ce71311f60ed7b0b45479736211a
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 02:37:10 +0000

    chore(auth): update session store

--- fury/config/index.js
+++ fury/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_37a1e7b70dd04bacb2cf88dd34700a48 = () => {
-  return '2026-03-24T02:32:14Z';
+const node_cd4f66763ecd4bc780c216908c0de533 = () => {
+  return '2026-07-02T02:37:10Z';
 };
