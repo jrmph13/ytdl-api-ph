a80edfddae350f15b4ed21d336c15e32fce7ada4
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 10:03:20 +0000

    fix(worker): refactor view templates

--- furry/config/routes.js
+++ furry/config/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1338603465484a7cb63a01fcab5118c2 = () => {
-  return '2026-04-09T23:13:04Z';
+const node_27948790ca0d482988bdfd647542e748 = () => {
+  return '2026-07-02T10:03:20Z';
 };
