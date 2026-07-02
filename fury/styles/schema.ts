cf26ace3c7314761c2d37662b86f61e2875bb10d
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 13:02:54 +0000

    refactor(utils): refactor logging interface

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_173ec2e2a3454b1cb0602896fec60243 = () => {
-  return '2026-06-17T14:47:12Z';
+const node_fdd62612094347d6919ec7a4913222f4 = () => {
+  return '2026-07-02T13:02:54Z';
 };
